96c4833a2d260ec9618571eea45cfb92fdba1336
Jeganathan <jegan@tektutor.org>
2022-12-05 20:30:35 -0800

    Added Day1 lab exercises.

--- Day1/create-docker-container-using-terraform/main.tf
+++ Day1/create-docker-container-using-terraform/main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.23.1"
-    }
-  }
-}
-
-provider "docker" {
-}
-
 resource "docker_image" "ubuntu_image" {
    name = "ubuntu:22.04"
    keep_locally = false
@@ -17,6 +5,12 @@ resource "docker_image" "ubuntu_image" {
 
 resource "docker_container"  "ubuntu-container" {
    name = "c1"
+   image = docker_image.ubuntu_image.image_id
+   command = [ "/bin/bash", "-c", "sleep 9999999" ] 
+}
+
+resource "docker_container"  "ubuntu-container2" {
+   name = "c2"
    image = docker_image.ubuntu_image.image_id
    command = [ "/bin/bash", "-c", "sleep 9999999" ] 
 }
