dee145adaf4be6b477c34c65d589688af6270f70
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-12-07 18:22:56 +0530

    Added Day3 lab exercises.

---
+++ Day3/invoke-ansible-playbook-from-terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {
+  type = string
+  default = "tektutor-resource-group-new"
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vm_count" {
+  type = number
+}
