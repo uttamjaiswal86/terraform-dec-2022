be08f7240e9582d3df2686e1759ead1efb623694
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-12-07 12:59:12 +0530

    Added lab exercise that demonstrates aks cluster creation using terraform.

---
+++ Day3/provision-aks-cluster/variables.tf
@@ -0,0 +1,13 @@
+variable "cluster_name" {
+  type = string
+  default = "tektutor-aks-cluster"
+}
+
+variable "resource_group_name" {
+  type = string
+  default = "tektutor-resource-group-5"
+}
+
+variable "location" {
+  default = "eastus"
+}
