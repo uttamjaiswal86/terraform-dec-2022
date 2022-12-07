34ef311a0e8bd18e92d12a4db5582170e6a97981
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-12-07 10:19:20 +0530

    Added Day3 lab exercises.

---
+++ Day3/loadbalancer/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {
+  type = string
+  default = "tektutor-resource-group-2"
+}
+
+variable "location" {
+  type = string
+  default = "eastus"
+}
+
+variable "vm_count" {
+  type = number
+}
