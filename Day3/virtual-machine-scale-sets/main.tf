2bf7550ba280f9be3630f8d7fd887bf7cc0d1322
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-12-07 14:53:09 +0530

    Added machine scale set lab exercise.

---
+++ Day3/virtual-machine-scale-sets/main.tf
@@ -0,0 +1,58 @@
+resource "azurerm_resource_group" "rg" {
+   name = "${var.prefix}-resource-group"
+   location = "${var.location}"
+}
+
+resource "azurerm_virtual_network" "network" {
+    name = "${var.prefix}-network"
+    
+    address_space = ["10.0.0.0/16"]
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "internal" {
+   name = "internal"
+   resource_group_name = azurerm_resource_group.rg.name
+   
+   virtual_network_name = azurerm_virtual_network.network.name
+
+   address_prefixes = ["10.0.2.0/24"]
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "machine_scale_set" {
+
+   name = "${var.prefix}-vmss" 
+   location = azurerm_resource_group.rg.location
+   resource_group_name = azurerm_resource_group.rg.name
+
+   sku = "Standard_F2"
+   instances = 3
+   admin_username = "azureuser"
+   admin_password = "password@123"
+   disable_password_authentication = false
+
+   source_image_reference {
+      publisher = "Cononical"
+      offer = "UbuntuServer"
+      sku = "18.04-LTS"
+      version = "latest"
+   }
+
+   network_interface {
+      name = "network"
+      primary = true 
+
+      ip_configuration {
+         name = "internal"
+         primary = true
+         subnet_id = azurerm_subnet.internal.id
+      }
+   }
+
+   os_disk {
+      storage_account_type = "Standard_LRS"
+      caching = "ReadWrite"
+   }
+   
+}
---
+++ Day3/virtual-machine-scale-sets/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "9eb2e122-36af-4b34-9186-2cc8053669e3" #Replace this subscription_id with your Azure pass subscription id
+}
