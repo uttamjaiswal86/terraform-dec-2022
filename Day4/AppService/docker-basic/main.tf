503073ef5252f5e1b3523ad4ab26d397ed6c89b0
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-12-08 16:20:57 +0530

    Added AppService lab exercises to Day4.

---
+++ Day4/AppService/docker-basic/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_resource_group" "tektutor" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "tektutor" {
+  name                = "${var.prefix}-sp-zipdeploy"
+  location            = azurerm_resource_group.tektutor.location
+  resource_group_name = azurerm_resource_group.tektutor.name
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+
+resource "azurerm_linux_web_app" "tektutor" {
+  name                = "${var.prefix}-tektutor"
+  location            = azurerm_resource_group.tektutor.location
+  resource_group_name = azurerm_resource_group.tektutor.name
+  service_plan_id     = azurerm_service_plan.tektutor.id
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  site_config {
+    application_stack {
+      docker_image     = "tektutor/hello-ms"
+      docker_image_tag = "3.0"
+    }
+  }
+}
