53ed71efb52e2539b01dcc714bccb4440323b294
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-12-09 16:23:58 +0530

    Added lab exercise that demonstrates storing terraform state in azure storage account.

--- Day5/centrally-stored-terraform-state/providers.tf
+++ Day5/centrally-stored-terraform-state/providers.tf
@@ -4,20 +4,15 @@ terraform {
       source = "hashicorp/azurerm"
       version = "3.33.0"
     }
-
-    tls = {
-      source = "hashicorp/tls"
-      version = "4.0.4"
-    }
   }
-/**/
   backend "azurerm" {
-     resource_group_name = "tektutor-resource-group-new"
-     storage_account_name = "tektutortfstate" 
-     container_name = "tektutortfstate"
-     key = "terraform.tfstate"
+     resource_group_name = "centralized-state-rg"
+     storage_account_name = "jegantfstate" 
+     container_name = "jegantfstate"
+     key = "tektutor"
+     #Replace this subscription_id with your Azure pass subscription id
+     subscription_id = "9eb2e122-36af-4b34-9186-2cc8053669e3" 
   }
-/**/
 }
 
 provider "azurerm" {
